f6b75a88d4fd8bc0fb427a1ff3600f988ae2916b
Brian Bravol <lumalabravo@gmail.com>
2024-11-11 10:37:15 +0300

    creating react components

--- course-one/moviedux/src/App.js
+++ course-one/moviedux/src/App.js
@@ -1,5 +1,15 @@
+import "./styles.css";
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <header className="header">
+        <h1>Welcome to Movie Dux</h1>
+      </header>
+      <footer className="footer">
+        <p className="footer">Footer content here</p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
